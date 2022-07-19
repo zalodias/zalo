fac9b7363d773c68261ac3a78a4d487029da07b0
Gonçalo Dias <goncalodias.multimedia@gmail.com>
2022-07-19 15:21:35 +0100

    🎉 Initial commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Gonçalo Dias</title>
+      </Head>
+    </div>
+  );
+};
+
+export default Home;
