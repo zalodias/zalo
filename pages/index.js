5f3c1f382baf2e17e61189ba5162877bf89874a6
Gonçalo Dias <goncalodias.multimedia@gmail.com>
2022-07-20 15:32:24 +0100

    ✨ Add social link icons

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
 import Head from "next/head";
 
+import {
+  TwitterLogo,
+  GithubLogo,
+  LinkedinLogo,
+  MediumLogo,
+  FigmaLogo,
+  DribbbleLogo,
+  At,
+} from "phosphor-react";
+
 const Home = () => {
   return (
     <div className="">
@@ -12,6 +22,29 @@ const Home = () => {
         <p className="text-lg max-w-prose">
           Welcome to my piece of the internet.
         </p>
+        <div className="flex gap-4 text-stone-100">
+          <a href="" target="_blank">
+            <TwitterLogo size={24} />
+          </a>
+          <a href="" target="_blank">
+            <GithubLogo size={24} />
+          </a>
+          <a href="" target="_blank">
+            <LinkedinLogo size={24} />
+          </a>
+          <a href="" target="_blank">
+            <MediumLogo size={24} />
+          </a>
+          <a href="" target="_blank">
+            <FigmaLogo size={24} />
+          </a>
+          <a href="" target="_blank">
+            <DribbbleLogo size={24} />
+          </a>
+          <a href="" target="_blank">
+            <At size={24} />
+          </a>
+        </div>
       </header>
     </div>
   );
