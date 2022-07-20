16d40814468f70a62341f3c7206e0a1847cc7cc7
Gonçalo Dias <goncalodias.multimedia@gmail.com>
2022-07-20 14:21:38 +0100

    🔧 Add font weights and base styles to next/document

--- pages/_document.js
+++ pages/_document.js
@@ -5,19 +5,11 @@ export default function Document() {
     <Html>
       <Head>
         <link
-<<<<<<< HEAD
           href="https://fonts.googleapis.com/css2?family=Albert+Sans:wght@200;400;600;800&display=optional"
           rel="stylesheet"
         />
       </Head>
       <body className="bg-stone-900 text-stone-400 px-8 py-16 max-w-screen-lg mx-auto">
-=======
-          href="https://fonts.googleapis.com/css2?family=Albert+Sans&display=optional"
-          rel="stylesheet"
-        />
-      </Head>
-      <body className="bg-neutral-800 text-neutral-400">
->>>>>>> 4cf4833 (🔧 Add custom next/document)
         <Main />
         <NextScript />
       </body>
