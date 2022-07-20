6146117ef9dc1f2eb545590559df48a1e79b2b7f
Gonçalo Dias <goncalodias.multimedia@gmail.com>
2022-07-20 14:37:22 +0100

    ✨ Add header section

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,13 @@ const Home = () => {
       <Head>
         <title>Gonçalo Dias</title>
       </Head>
+      <header className="flex flex-col gap-4">
+        <img src="/images/headshot.webp" alt="" className="rounded-full w-20" />
+        <h1 className="font-semibold text-2xl text-stone-100">Gonçalo Dias</h1>
+        <p className="text-lg max-w-prose">
+          Welcome to my piece of the internet.
+        </p>
+      </header>
     </div>
   );
 };
