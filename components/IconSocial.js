68feb39f8dc0d1275952c922d95bf25e8196907c
Gonçalo Dias <goncalodias.multimedia@gmail.com>
2022-07-22 20:02:22 +0100

    ✨ Add IconSocial component

---
+++ components/IconSocial.js
@@ -0,0 +1,13 @@
+const IconSocial = ({ icon }) => {
+  return (
+    <a
+      href=""
+      target="_blank"
+      className="p-2 rounded-full transition hover:bg-stone-800"
+    >
+      {icon}
+    </a>
+  );
+};
+
+export default IconSocial;
