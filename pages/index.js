221b13eedc91927ccd6e5304e2184ebc17b8bece
Gonçalo Dias <goncalodias.multimedia@gmail.com>
2022-07-22 20:06:28 +0100

    ✨ Update page header

--- pages/index.js
+++ pages/index.js
@@ -16,35 +16,35 @@ const Home = () => {
       <Head>
         <title>Gonçalo Dias</title>
       </Head>
-      <header className="flex flex-col gap-4">
-        <img src="/images/headshot.webp" alt="" className="rounded-full w-20" />
-        <h1 className="font-semibold text-2xl text-stone-100">Gonçalo Dias</h1>
-        <p className="text-lg max-w-prose">
-          Welcome to my piece of the internet.
-        </p>
-        <div className="flex gap-4 text-stone-100">
-          <a href="" target="_blank">
-            <TwitterLogo size={24} />
-          </a>
-          <a href="" target="_blank">
-            <GithubLogo size={24} />
-          </a>
-          <a href="" target="_blank">
-            <LinkedinLogo size={24} />
-          </a>
-          <a href="" target="_blank">
-            <MediumLogo size={24} />
-          </a>
-          <a href="" target="_blank">
-            <FigmaLogo size={24} />
-          </a>
-          <a href="" target="_blank">
-            <DribbbleLogo size={24} />
-          </a>
-          <a href="" target="_blank">
-            <At size={24} />
-          </a>
+      <header className="flex flex-col gap-8">
+        <div className="flex flex-col gap-4">
+          <img
+            src="/images/headshot.webp"
+            alt=""
+            className="rounded-full w-20"
+          />
+          <h1 className="font-semibold text-2xl text-stone-100">
+            Gonçalo Dias
+          </h1>
+          <p className="text-lg max-w-prose">
+            Welcome to my piece of the internet.
+          </p>
+          <p className="text-lg max-w-prose">
+            I'm mainly focused on exploring the intersection between art, design
+            and technology. Most of what you'll see here are the results of that
+            exploration: whether in the form of thoughts, words or actions.
+          </p>
         </div>
+        <div className="flex gap-4 text-stone-100">
+          <IconSocial icon={<TwitterLogo size={24} />} />
+          <IconSocial icon={<GithubLogo size={24} />} />
+          <IconSocial icon={<LinkedinLogo size={24} />} />
+          <IconSocial icon={<MediumLogo size={24} />} />
+          <IconSocial icon={<FigmaLogo size={24} />} />
+          <IconSocial icon={<DribbbleLogo size={24} />} />
+          <IconSocial icon={<At size={24} />} />
+        </div>
+      </header>
       </header>
     </div>
   );
