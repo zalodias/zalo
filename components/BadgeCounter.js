2d74195f0194f70a74916473af279ab14c459487
Gonçalo Dias <goncalodias.multimedia@gmail.com>
2022-07-24 16:26:25 +0100

    ✨ Add BadgeCounter component

---
+++ components/BadgeCounter.js
@@ -0,0 +1,9 @@
+const BadgeCounter = ({ number }) => {
+  return (
+    <div className="inline-block bg-teal-500 px-2 py-3 text-xs font-semibold text-stone-100 leading-[0] rounded-full">
+      {number}
+    </div>
+  );
+};
+
+export default BadgeCounter;
