0a172144859fa650451ae7dccd2069e723d6fdf2
Gonçalo Dias <goncalodias.multimedia@gmail.com>
2022-07-24 18:07:47 +0100

    📱 Improve responsive design of CardGrid component

--- components/CardGrid.js
+++ components/CardGrid.js
@@ -1,6 +1,6 @@
 const CardGrid = ({ children }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {children}
     </div>
   );
