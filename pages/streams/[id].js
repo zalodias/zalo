72fabfac992569f0eefee433c56145e342aba3d3
Gonçalo Dias <goncalodias.multimedia@gmail.com>
2022-07-28 10:47:06 +0100

    :lipstick: Render styled text blocks on UI

--- pages/streams/[id].js
+++ pages/streams/[id].js
@@ -1,6 +1,27 @@
 import Head from "next/head";
+import { Fragment } from "react";
 import { getDatabase, getPage, getBlocks } from "../../lib/notion";
 import { databaseId } from "../index";
+
+const renderBlock = (block) => {
+  const { type } = block;
+  const value = block[type];
+
+  switch (type) {
+    case "paragraph":
+      return (
+        <div className="bg-stone-700 text-stone-100 p-2 rounded-lg max-w-sm self-start">
+          {value.rich_text[0].plain_text}
+        </div>
+      );
+    case "quote":
+      return (
+        <div className="bg-blue-600 text-stone-100 p-2 rounded-lg max-w-sm self-end">
+          {value.rich_text[0].plain_text}
+        </div>
+      );
+  }
+};
 
 export default function Stream({ page, blocks }) {
   return (
@@ -8,6 +29,11 @@ export default function Stream({ page, blocks }) {
       <Head>
         <title>{page.properties.Name.title[0].text.content}</title>
       </Head>
+      <section className="flex flex-col gap-2">
+        {blocks.map((block) => (
+          <Fragment key={block.id}>{renderBlock(block)}</Fragment>
+        ))}
+      </section>
     </div>
   );
 }
